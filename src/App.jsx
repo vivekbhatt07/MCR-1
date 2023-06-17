d839ee20b247353536549be63e38fff1238a294c
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-06-17 18:12:15 +0530

    style: reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import { Home, About } from "./Pages";
+
 function App() {
-  return <div className="app">Hello World</div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
